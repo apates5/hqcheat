a8565fca9e777adb0e5471be149f0a16fa3f0c3f
apates5 <31677811+apates5@users.noreply.github.com>
2018-01-08 17:51:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const processImage = path => {
   const top = 300;
   const bottom = 1150;
   const left = 135;
+  const right = 135;
   var config1 = { width: 1174 - left - left, height: 2278 - top - bottom, top, left };
   console.log('\033c')
   console.log('Processing...');
