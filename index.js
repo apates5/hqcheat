c651ea1922151cf18070bae9ff0e3be8749020c4
apates5 <31677811+apates5@users.noreply.github.com>
2018-01-08 17:42:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ const occurrences = (string, subString, allowOverlapping) => {
 }
 
 const processImage = path => {
-  const top = 400;
+  const top = 250;
   const bottom = 1150;
-  const left = 140;
+  const left = 150;
   var config1 = { width: 1174 - left - left, height: 2278 - top - bottom, top, left };
   console.log('\033c')
   console.log('Processing...');
